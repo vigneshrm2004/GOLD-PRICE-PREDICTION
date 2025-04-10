ad4ebc8f895b0a03db3c5a7a7c2c0d07a2a61e5b
Vignesh RM <136584779+vigneshrm2004@users.noreply.github.com>
2025-04-10 09:48:35 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ModelComparison from './components/ModelComparison';
+
+function App() {
+  return (
+    <div className="App">
+      <ModelComparison />
+    </div>
+  );
+}
+
+export default App;
